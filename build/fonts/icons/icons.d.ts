bed12cc9409d2a614c475aed9ddd50e81ab0e6ad
ponyal <128210461+zimoyholodno@users.noreply.github.com>
2026-06-01 22:08:27 +0400

    feat: начало

---
+++ build/fonts/icons/icons.d.ts
@@ -0,0 +1,17 @@
+export enum Icons {
+  AccordionChevron = "icon-accordion-chevron",
+  AccordionPlus = "icon-accordion-plus",
+  Attachment = "icon-attachment",
+  Burger = "icon-burger",
+  Chevron = "icon-chevron",
+  Clear = "icon-clear",
+  Cross = "icon-cross",
+  Mail = "icon-mail",
+  Phone = "icon-phone",
+  Telegram = "icon-telegram",
+  Vk = "icon-vk",
+  Whatsapp = "icon-whatsapp",
+}
+export type IconsClassname = "icon-accordion-chevron" | "icon-accordion-plus" | "icon-attachment" | "icon-burger" | "icon-chevron" | "icon-clear" | "icon-cross" | "icon-mail" | "icon-phone" | "icon-telegram" | "icon-vk" | "icon-whatsapp"
+export type IconsIcon = "accordion-chevron" | "accordion-plus" | "attachment" | "burger" | "chevron" | "clear" | "cross" | "mail" | "phone" | "telegram" | "vk" | "whatsapp"
+export const IconsPrefix = "icon-"
